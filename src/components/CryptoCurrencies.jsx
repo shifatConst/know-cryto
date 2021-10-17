f3b5cf934e27835f1200962c652502b9902393a6
Shifat Reza <shifatreza44@gmail.com>
2021-10-17 17:58:05 +0600

    Layout done

---
+++ src/components/CryptoCurrencies.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CryptoCurrencies = () => {
+    return (
+        <div>
+            <h1>Crypto Currencies page</h1>
+        </div>
+    );
+};
+
+export default CryptoCurrencies;

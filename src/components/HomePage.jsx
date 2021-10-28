a9e24fb1a4e11f9886b798e9f21c5b7746018cd9
Shifat Reza <shifatreza44@gmail.com>
2021-10-28 12:08:57 +0600

    redux-toolkit

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import millify from 'millify';
 import { Typography, Row, Col, Statistic } from 'antd';
+import { useGetCryptosQuery } from '../services/cryptoApi';
 
 const { Title } = Typography;
 const HomePage = () => {
+
+    const { data, isFetching } = useGetCryptosQuery();
+    console.log(data);
+
     return (
         <>
             <Title level={2} className="heading">Global Crypto Stats</Title>
--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+
 import App from './App';
+import { Provider } from 'react-redux';
+import store from './app/store';
 import { BrowserRouter as Router } from 'react-router-dom';
 import 'antd/dist/antd.css';
 
 ReactDom.render(
     <Router>
-        <App />
+        <Provider store={store}>
+            <App />
+        </Provider>
     </Router>,
     document.getElementById('root')
 );
